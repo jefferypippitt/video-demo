c6cda7a24e2c03fad6a9e70c538be22fa3d5bc56
Jeffery <jefferyspippitt@gmail.com>
2024-04-13 11:28:58 -0500

    fixed mobile view

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,8 +7,7 @@ import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 import { Button } from './ui/button';
 import { Volume2, VolumeX } from 'lucide-react';
 
-const words = `Vercel empowers developers with lightning-fast deployment and seamless scalability. With its intuitive platform, building and deploying modern web applications has never been easier. `
-
+const words = `Vercel empowers developers with lightning-fast deployment and seamless scalability. With its intuitive platform, building and deploying modern web applications has never been easier. `;
 
 const HeroSection: React.FC = () => {
   const [muted, setMuted] = useState(true);
@@ -21,19 +20,15 @@ const HeroSection: React.FC = () => {
     <section className="relative h-screen flex flex-col justify-center">
       <div className='relative flex h-screen flex-col justify-center'>
         <div className='absolute inset-0 -z-10'>
-
-          <div>
-            <NextVideo
-              src={sample}
-              className='object-cover w-full h-full'
-              autoPlay
-              loop
-              muted={muted} // Use muted state variable to control muting
-            />
-          </div>
-          {/* Mute/Unmute Button */}
-          
+          <NextVideo
+            src={sample}
+            className='object-cover w-full h-full'
+            autoPlay
+            loop
+            muted={muted} // Use muted state variable to control muting
+          />
         </div>
+        {/* Mute/Unmute Button */}
         <div className='justify-top absolute inset-0 mt-24 flex flex-col items-center text-center'>
           <div className='flex items-center gap-4'>
             <Button variant={'secondary'} onClick={handleToggleMute} className={`relative inline-block rounded-full h-12 w-12 transition-all duration-300`}>
@@ -43,8 +38,9 @@ const HeroSection: React.FC = () => {
               </div>
             </Button>
             <TextGenerateEffect
-              className='max-w-[20ch] text-center text-6xl md:text-4xl lg:text-6xl tracking-tight first:mt-0 font-thin'
+              className='text-center text-6xl max-w-[20ch] md:text-4xl lg:text-6xl tracking-tight first:mt-0 font-thin'
               words={words}
+             
             />
           </div>
         </div>
