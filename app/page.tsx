20557a3dc347f623387d02f56b3a99e70c9c96c0
Jeffery <jefferyspippitt@gmail.com>
2024-04-13 12:58:41 -0500

    added github button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Footer from '@/components/footer';
+import Footer from '@/components/Footer'
 import HeroSection from '@/components/HeroSection'
 
 import Navbar from '@/components/Navbar'
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,30 @@
-"use client";
+'use client';
 
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import { ModeToggle } from './ui/theme-toggle';
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { ModeToggle } from './ui/theme-toggle'
+import { Github } from 'lucide-react'
+import { Button } from './ui/button'
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
+    setMenuOpen(!menuOpen)
+  }
 
   return (
-    <nav className='flex flex-col md:flex-row items-center justify-between p-4 px-6 md:px-20'>
-      <div className='flex items-center justify-between w-full md:w-auto'>
+    <nav className='flex flex-col items-center justify-between p-4 px-6 md:flex-row md:px-20'>
+      <div className='flex w-full items-center justify-between md:w-auto'>
         <div className='flex items-center gap-6'>
           <Image src='/logo.svg' alt='logo' width={150} height={150} />
         </div>
-        <div className='md:hidden flex'>
+        <div className='flex md:hidden'>
           <button onClick={handleMenuToggle} className='focus:outline-none'>
             <svg
-              className='w-6 h-6'
+              className='h-6 w-6'
               fill='none'
               stroke='currentColor'
               viewBox='0 0 24 24'
@@ -49,26 +52,33 @@ export default function Navbar() {
       <div
         className={`${
           menuOpen ? 'block' : 'hidden'
-        } md:flex flex-col md:flex-row md:items-center md:gap-6 w-full md:w-auto`}
+        } w-full flex-col md:flex md:w-auto md:flex-row md:items-center md:gap-6`}
       >
-        <div className='flex flex-col md:flex-row items-center gap-6 mt-4'> {/* Added mt-4 class here */}
-          <Link href='/' className='font-medium'>
+        <div className='mt-4 flex flex-col items-center gap-6 md:flex-row'>
+          {' '}
+          {/* Added mt-4 class here */}
+          <Link
+            href='/'
+            className='font-medium underline-offset-4 hover:underline'
+          >
             Home
           </Link>
-          <Link href='#' className='font-medium'>
-            Docs
-          </Link>
-          <Link href='#' className='font-medium'>
-            Blog
-          </Link>
-          <Link href='#' className='font-medium'>
+          <Link
+            href='#'
+            className='font-medium underline-offset-4 hover:underline'
+          >
             Contact
           </Link>
-          <div className='flex items-center'>
+          <div className='flex items-center gap-4'>
             <ModeToggle />
+            <Button variant='outline' size='icon'>
+              <Link href='https://github.com/jefferypippitt' target='_blank'>
+                <Github />
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
     </nav>
-  );
+  )
 }
